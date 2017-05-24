58d319d71ec867faedaa43a80dbe0a8d036dbe88
Flavio Rossi <flaviowenzel1996@gmail.com>
2017-05-24 01:20:12 -0300

    Instituição corrigida para atender o banco de dados

--- src/main/java/br/com/pap5/bo/Instituicao.java
+++ src/main/java/br/com/pap5/bo/Instituicao.java
@@ -1,21 +1,15 @@
 package br.com.pap5.bo;
 
 import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
 @NamedQueries({
@@ -45,23 +39,10 @@ public class Instituicao implements Serializable {
     private String telcontat1;
     @Column(length = 12)
     private String telcontat2;
-    @Column(nullable = false)
-    @Temporal(TemporalType.DATE)
-    private Date datainscricao;
     @Column(length = 500)
     private String descricao;
-    @Column(nullable = false)
-    @Temporal(TemporalType.DATE)
-    private Date datavalidacao;
-    @Column(nullable = false)
-    @Temporal(TemporalType.DATE)
-    private Date datavalidade;
     @Column(length = 40)
     private String contabancaria;
-    
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "idusuario", referencedColumnName = "id")
-    private Usuario usuario;
 
     public Long getId() {
         return id;
@@ -95,20 +76,8 @@ public class Instituicao implements Serializable {
         return telcontat2;
     }
 
-    public Date getDatainscricao() {
-        return datainscricao;
-    }
-
     public String getDescricao() {
         return descricao;
-    }
-
-    public Date getDatavalidacao() {
-        return datavalidacao;
-    }
-
-    public Date getDatavalidade() {
-        return datavalidade;
     }
 
     public String getContabancaria() {
@@ -146,22 +115,11 @@ public class Instituicao implements Serializable {
     public void setTelcontat2(String telcontat2) {
         this.telcontat2 = telcontat2;
     }
-
-    public void setDatainscricao(Date datainscricao) {
-        this.datainscricao = datainscricao;
-    }
-
+    
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
-    public void setDatavalidacao(Date datavalidacao) {
-        this.datavalidacao = datavalidacao;
-    }
-
-    public void setDatavalidade(Date datavalidade) {
-        this.datavalidade = datavalidade;
-    }
 
     public void setContabancaria(String contabancaria) {
         this.contabancaria = contabancaria;
